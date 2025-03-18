f1f0e9ed15022047270d35975e054640f33050a1
David Nyaga <105038549+Nyagatech@users.noreply.github.com>
2025-03-18 12:32:31 +0300

    created the hero and the product card

---
+++ components/product.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <div class="carousel carousel-center rounded-md m-4 gap-8 ">
+      <div v-for="item in carouselItems" :key="item.id" class="carousel-item relative">
+        <img :src="item.imageUrl" :alt="item.name" class="w-full" />
+        <div class="absolute top-4 left-4 text-white text-start">
+          <h3 class="text-xl font-semibold">{{ item.name }}</h3>
+          <p class="text-sm">{{ item.description }}</p>
+        </div>
+        <div class="absolute top-4 right-4">
+            <button class="btn btn-circle btn-sm">
+                <Heart />
+            </button>
+        </div>
+        <div class="absolute bottom-4 left-4 text-white">
+          <p class="text-lg font-semibold">${{ item.price }}</p>
+        </div>
+        <div class="absolute bottom-4 right-4">
+          <button class="btn btn-sm bg-[#5f2121] text-white hover:bg-[#722929]">Add to Cart</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { Search, Heart, ShoppingBag } from 'lucide-vue-next';
+const carouselItems = [
+  {
+    id: 1,
+    imageUrl: 'https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp',
+    name: 'Delicious Pizza',
+    description: 'A classic pizza with fresh toppings.',
+    price: 12.99,
+  },
+  {
+    id: 2,
+    imageUrl: 'https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp',
+    name: 'Tasty Burger',
+    description: 'A juicy burger with all the fixings.',
+    price: 9.99,
+  },
+  {
+    id: 3,
+    imageUrl: 'https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp',
+    name: 'Fresh Salad',
+    description: 'A healthy salad with mixed greens.',
+    price: 7.99,
+  },
+  {
+    id: 4,
+    imageUrl: 'https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.webp',
+    name: 'Spaghetti',
+    description: 'Delicious Spaghetti with meat sauce.',
+    price: 11.99,
+  },
+  {
+    id: 5,
+    imageUrl: 'https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp',
+    name: 'Sushi',
+    description: 'Fresh and delicious sushi rolls.',
+    price: 15.99,
+  },
+  {
+    id: 6,
+    imageUrl: 'https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.webp',
+    name: 'Steak',
+    description: 'Perfectly cooked steak with sides.',
+    price: 19.99,
+  },
+  {
+    id: 7,
+    imageUrl: 'https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp',
+    name: 'Ice Cream',
+    description: 'Refreshing ice cream with toppings.',
+    price: 5.99,
+  },
+];
+</script>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,82 @@
 <template>
   <div>
     <div class="sticky top-0 z-50">
-    <Navbar />
+      <Navbar />
     </div>
-     <section id="hero">
-      <div class="hero bg- min-h-screen">
-  <div class="hero-content flex-col lg:flex-row-reverse">
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-      class="max-w-sm rounded-lg shadow-2xl" />
-    <div>
-      <h1 class="text-5xl font-bold">Box Office News!</h1>
-      <p class="py-6">
-        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-        quasi. In deleniti eaque aut repudiandae et a id nisi.
-      </p>
-      <button class="btn btn-primary">Get Started</button>
-    </div>
-  </div>
-</div>
-     </section>
+    <section id="hero">
+      <div class="hero min-h-screen">
+        <div class="hero-content flex-col lg:flex-row-reverse">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+            class="max-w-lg rounded-lg shadow-2xl"
+          />
+          <div>
+            <h1 class="text-5xl font-bold text-[#5f2121]">Homey Collection 2025</h1>
+            <p class="py-6">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.
+              Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies
+              sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a,
+              semper congue, euismod non, mi.
+            </p>
+            <div class="flex space-x-4">
+              <router-link to="/product">
+                <button class="btn rounded-full border border-gray-50">
+                  Start shopping
+                </button>
+              </router-link>
+
+              <button class="btn bg-[#5f2121] hover:bg-[#722929] text-white">
+                Learn More
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section>
+      <div class="text-center py-10">
+        <h2 class="text-3xl text-[#5f2121] font-bold mb-8">
+          Choose The Category <br />
+          That You Want
+        </h2>
+        <div
+          class="flex flex-wrap bg-base-300 w-full sm:w-4/5 md:w-3/4 lg:w-1/3 rounded-full justify-center gap-4 py-4 mx-auto"
+        >
+          <button
+            v-for="c in categories"
+            :key="c.name"
+           class=":hover:text-gray-500 "
+           :class="c.name === 'All Furniture' ? 'text-[#5f2121] font-bold' : 'text-gray-300'"
+          >
+            {{ c.name }}
+          </button>
+        </div>
+        <div class="">
+          <Product />
+        </div>
+
+        
+      </div>
+
+    </section>
   </div>
 </template>
 
 <script setup>
+const categories = [
+  {
+    name: 'All Furniture',
+  },
+  {
+    name: 'Living Room',
+  },
+  {
+    name: 'Kitchen',
+  },
+  {
+    name:'Bedroom'
+  }
+];
 </script>
 
 <style></style>
